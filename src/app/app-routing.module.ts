ec178089373bcf640edb7dd94e0c47ba01f3eb6e
Stuart Brown <stuart@Sauds-Macbook.local>
2024-02-13 01:15:35 +0400

    initial commit
    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from 'src/app/Components/header/header.component';
+import { LoginComponent } from 'src/app/modals/login/login.component';
+import { LayoutComponent } from 'src/app/shared/layout/layout.component';
+import { RedTeamComponent } from './shared/red-team/red-team.component';
+
+
+export const routes: Routes = [
+
+  {
+    path: '',
+    component: LayoutComponent
+  },
+  {
+    path: 'blue-team',
+    loadComponent: () => import('./shared/blue-team/blue-team.component').then(m => m.BlueTeamComponent),
+  },
+  {
+    path: 'red-team',
+    loadComponent: () => import('./shared/red-team/red-team.component').then(m => m.RedTeamComponent),
+  },
+  {
+    path: 'compete',
+    loadComponent: () => import('./shared/compete/compete.component').then(m => m.CompeteComponent),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { LayoutComponent } from './shared/layout/layout.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+
+
+})
+export class AppComponent {
+  title = 'tryHackMe_1.0';
+
+  
+}
